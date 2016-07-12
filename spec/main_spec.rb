4345fb241df2de64d2c8a75edb335d8235639d03
Teerapong ladlee <teerapong.l@gbtech.co.th>
2016-07-12 18:57:48 +0700

    Add test playbook

---
+++ spec/main_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "System spec" do
+    describe "Required packages should be installed" do
+        describe package('sshfs') do
+            it { should be_installed }
+        end
+    end
+end
+
+mounted_directory = '/mnt/sshfs/mainbox/logs'
+syslog_file = "#{mounted_directory}/syslog"
+supervisor_program = 'supervisor_sshfs_mainbox'
+
+describe "Tasks spec" do
+    describe "Given directory should be mounted directory" do
+        describe file("#{mounted_directory}") do
+              it { should be_mounted }
+        end
+    end
+    describe "Syslog file from mounted directory should be exist" do
+        describe file("#{syslog_file}") do
+              it { should be_file }
+        end
+    end
+    describe service("#{supervisor_program}") do
+          it { should be_running.under('supervisor') }
+    end
+end
